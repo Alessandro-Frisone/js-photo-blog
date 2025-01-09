6980475040128c83c5e43eaba25fd18972a60a53
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-09 14:50:01 +0100

    feat: milestone2/ part of 3

--- js/script.js
+++ js/script.js
@@ -3,13 +3,24 @@ const cardElm = document.getElementById("card");
 
 // FUNCTIONS
 function addCard() {
-    for (let i = 1; i < 6; i++){
-        axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then(function (resp) {
-            console.log(resp);
-        })
-    }
+            const newCard = resp.data; 
+            newCard.forEach(card => {
+                const title = card.title;
+                const immage = card.url;
+                cardElm.innerHTML += `
+                    <div class="col bg-light mt-3" id="card">
+                        <img src="${immage}" alt="img">
+                        <p class="mt-2">${title}</p>
+                    </div>
+                `;
+            });
+        });
 }
-    
+
+// PAGE LOAD
+addCard();
+
 
 // EVENTS
