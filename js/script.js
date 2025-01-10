8ba8855d1ab8a77099f45f3e74e37c46083f6a14
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-10 15:05:54 +0100

    feat: change img overaly with card img

--- js/script.js
+++ js/script.js
@@ -15,12 +15,31 @@ function addCard() {
                     <div class="col-lg-4 col-sm-6 col-12 p-2 card_rot mt-5" id="inf_card">  
                         <div class="bg-light mt-3 p-3 ms-shadow">
                             <img class="pin" src="./img/pin.svg" alt="Puntina">
-                            <img src="${immage}" alt="img">
+                            <img id="imgCard" src="${immage}" alt="img">
                             <p class="mt-2">${title}</p>
                         </div>
-                    </div>
-                `;
+                    </div>`;
             });
+
+            //Seleziono tutte le card (funziona sincrona)
+            const postCard = document.querySelectorAll("#inf_card");  
+            // Per ogni singola card aggiungo un evento
+            postCard.forEach((postCards) => {
+                // Applico l'evento click
+                postCards.addEventListener("click", () => {
+                    // Prendo l'informazione che mi interessa
+                    const onlyImg = postCards.querySelector("#imgCard")
+                    console.log(onlyImg)
+                    // Passo l'immagine dell'overlay
+                    const overlayImgElm = document.getElementById("overlay_img")
+                    // passo l'immagine della card
+                    const overlayImg = postCard.querySelector("imgCard").src 
+                    // Scambia l'src dell'immagine della card con quella dell'overlay
+                    overlayImgElm.src = overlayImg
+                    // Faccio compraire l'overlay dell'immagine selezionata
+                    overlayElm.classList.remove("hidden");
+                })
+            })
         });
 }
 
@@ -29,6 +48,3 @@ addCard();
 
 
 // EVENTS
-closeBtnElm.addEventListener("click", () => {
-    cardElm.classList.remove("hidden")
-})
